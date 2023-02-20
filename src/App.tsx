ab5de061fab91ac04f9af8468d1daf74566bbf19
Mahmoud <barghoutmahmoud2@gmail.com>
2023-02-20 18:49:04 +0200

    added a connection to a firestore database on every mount there is a check if a database exists and if so it is used to set all the items in shop as well as well as the items in cart. this basic functionality maintains the items in shopping cart between visits.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route, Form } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header";
 import Homepage from "./components/homepage";
 import Shop from "./components/shop";
@@ -14,18 +14,94 @@ import Gundam6 from "./resources/6.jpg";
 import Gundam7 from "./resources/7.jpg";
 import Gundam8 from "./resources/8.jpg";
 import Gundam9 from "./resources/9.jpg";
-
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  updateDoc,
+  doc,
+  setDoc,
+} from "firebase/firestore";
 const uniqid = require("uniqid");
 
-function App() {
-  interface itemCart {
-    name: string;
-    price: number;
-    image: string;
-    id: string;
-    quantity: number;
+const firebaseConfig = {
+  apiKey: "AIzaSyCEmeiYr_ckKZFNYcNkN9Mdql2qY5irfP4",
+  authDomain: "library-1c750.firebaseapp.com",
+  projectId: "library-1c750",
+  storageBucket: "library-1c750.appspot.com",
+  messagingSenderId: "978758902035",
+  appId: "1:978758902035:web:d0e461e02c2910379cbae6",
+};
+interface itemCart {
+  name: string;
+  price: number;
+  image: string;
+  id: string;
+  quantity: number;
+}
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function saveItems(item: itemCart) {
+  try {
+    await setDoc(doc(db, "items", item.id), {
+      name: item.name,
+      price: item.price,
+      image: item.image,
+      id: item.id,
+      quantity: item.quantity,
+    });
+  } catch (error) {
+    console.error("Error writing new message to Firebase Database", error);
   }
+}
+
+async function retrieveDB(allShopItems: itemCart[]) {
+  const querySnapshot = await getDocs(collection(db, "items"));
+  if (!querySnapshot.empty) {
+    let dataCollection: itemCart[] = [];
+    querySnapshot.forEach((doc) => {
+      let placeHolder: itemCart = {
+        name: doc.data().name,
+        price: doc.data().price,
+        image: doc.data().image,
+        id: doc.data().id,
+        quantity: doc.data().quantity,
+      };
+
+      dataCollection.push(placeHolder);
+    });
+    return dataCollection;
+  } else {
+    allShopItems.forEach((item) => saveItems(item));
+    return allShopItems;
+  }
+}
+
+async function updateQuantity(quantity: number, id: string) {
+  const itemRef = doc(db, "items", id);
+  await updateDoc(itemRef, { quantity: quantity });
+}
+
+function App() {
+  useEffect(() => {
+    const dataBase = retrieveDB(allShopItems);
+    if (dataBase) {
+      dataBase.then((value) => {
+        setAllShopItems(value);
+        setItemsInCart(value.filter((item) => item.quantity > 0));
+        setCartContent(
+          value.reduce(
+            (accumulator, currentValue) =>
+              accumulator + Number(currentValue.quantity),
+            0
+          )
+        );
+      });
+    }
+  }, []); // check if the database exists and if so use it as my source
   const [cartContent, setCartContent] = useState<number>(0);
   const [allShopItems, setAllShopItems] = useState<itemCart[]>([
     {
@@ -100,6 +176,7 @@ function App() {
     let itemsAdjusted = totalCartItems.find((item) => item.id === id);
     totalCartItems[totalCartItems.indexOf(itemsAdjusted!)].quantity = value;
     let addedItems = totalCartItems.filter((item) => item.quantity > 0);
+    updateQuantity(value, id);
 
     const numberOfItems =
       totalCartItems?.reduce(
